ca27f63dfd0a3e5e4ff432169f4733ed92652895
David A <dajatuewo@gmail.com>
2020-05-29 22:04:52 -0500

    Add project files.

---
+++ src/serilog-sinks-site24x7/sinks/Private/Sinks/TimeRolledDurableHttpSink.cs
@@ -0,0 +1,85 @@
+﻿// Copyright 2015-2019 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Text;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.Site24x7.Private.IO;
+using Serilog.Sinks.Site24x7.Private.Network;
+using Serilog.Sinks.RollingFile;
+
+namespace Serilog.Sinks.Site24x7.Private.Sinks
+{
+    /// <summary>
+    /// A durable sink that sends log events using HTTP POST over the network. A durable
+    /// sink will persist log events on disk in buffer files before sending them over the
+    /// network, thus protecting against data loss after a system or process restart. The
+    /// buffer files will use a rolling behavior based on time interval.
+    /// </summary>
+    /// <seealso cref="ILogEventSink" />
+    /// <seealso cref="IDisposable" />
+    public class TimeRolledDurableHttpSink : ILogEventSink, IDisposable
+    {
+        private readonly HttpLogShipper shipper;
+        private readonly ILogEventSink sink;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeRolledDurableHttpSink"/> class.
+        /// </summary>
+        public TimeRolledDurableHttpSink(
+            string requestUri,
+            string bufferPathFormat,
+            long? bufferFileSizeLimitBytes,
+            bool bufferFileShared,
+            int? retainedBufferFileCountLimit,
+            int batchPostingLimit,
+            TimeSpan period,
+            ITextFormatter textFormatter,
+            IBatchFormatter batchFormatter,
+            IHttpClient httpClient)
+        {
+            if (bufferFileSizeLimitBytes.HasValue && bufferFileSizeLimitBytes < 0)
+                throw new ArgumentOutOfRangeException(nameof(bufferFileSizeLimitBytes), "Negative value provided; file size limit must be non-negative.");
+
+            shipper = new HttpLogShipper(
+                httpClient,
+                requestUri,
+                new TimeRolledBufferFiles(new DirectoryService(), bufferPathFormat),
+                batchPostingLimit,
+                period,
+                batchFormatter);
+
+            sink = new RollingFileSink(
+                bufferPathFormat,
+                textFormatter,
+                bufferFileSizeLimitBytes,
+                retainedBufferFileCountLimit,
+                Encoding.UTF8,
+                shared: bufferFileShared);
+        }
+
+        /// <inheritdoc />
+        public void Emit(LogEvent logEvent) =>
+            sink.Emit(logEvent);
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            (sink as IDisposable)?.Dispose();
+            shipper?.Dispose();
+        }
+    }
+}
